7b61cefffefeb63e6f758c8ad753d02dbdada33a
AWeirdDev <90096971+AWeirdScratcher@users.noreply.github.com>
2022-06-06 19:00:09 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='mi_interact',
+    version='1.0',
+    packages=['mi_interact'],
+    license='MIT license',
+    long_description="用於 MineBot 交互的 Package",
+    author='AWeirdDev#1744',
+    author_email = "aweirdscrather@gmail.com",
+    install_requires=[
+    'discord'
+    ]
+)
